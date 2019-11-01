cbed4e104ecd4dee266bebc2c797b7532cbb99c8
mehtak1 <mehtak1@mcmaster.ca>
2019-10-31 21:34:39 -0400

    search page comments

--- js/searchPage.js
+++ js/searchPage.js
@@ -1,22 +1,30 @@
+/*This file contains the javascript for the search page*/
+
 $(document).ready(function(){
+  /*A click listener on the user location tab that fetches user location using Geolocation APi*/
     $("#userLocation-tab").click(function(){
         if (navigator.geolocation) {
+          /*Get the current position of the user*/
             navigator.geolocation.getCurrentPosition(onSuccess, onError);
         } else {
+          /*If not successful, display an error message*/
             $("#response-msg").html("<h4>Unfortunately, this feature is not available. Please choose another criteria to search by</h4>");
         }
     });
 
-    function onSuccess(position) {
-        console.log(position);
-        if (position) {
-            $(".spinner-container").hide();
-            $(".location-search").css("display", "flex");
-            $("#response-msg").html("<h4>You can select an option below to see all the best parks within a certain radius from you.</h4>");
-        }
+    /*Function that runs when geolocation is successfull*/
+    function onSuccess() {
+        /*Hide the div with the spinner*/
+        $(".spinner-container").hide();
+        /*Set css display properties for the div with class location search*/
+        $(".location-search").css("display", "flex");
+        /*Replace html in div with a success message and instructions*/
+        $("#response-msg").html("<h4>You can select an option below to see all the best parks within a certain radius from you.</h4>");
     }
 
+    /*Function that runs when there is an error*/
     function onError(error) {
+      /*Switch case statement for handling different error cases. Each case displays an error message*/
         switch(error.code) {
           case error.PERMISSION_DENIED:
             $("#response-msg").html("<h4>Unfortunately, this feature is not available. Please choose another criteria to search by</h4>");
